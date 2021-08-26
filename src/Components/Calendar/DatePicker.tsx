caa9d1b3b076b39001e0d42af0ef870bec7e06d0
nicrom06 <nicrom06@Protiviti.com>
2021-08-26 15:22:55 -0600

    changes

--- src/Components/Calendar/DatePicker.tsx
+++ src/Components/Calendar/DatePicker.tsx
@@ -1,25 +1,106 @@
 import moment from "moment";
-import React from "react";
+import { v4 } from "uuid";
+import React, { useState } from "react";
 import "./calendar.css";
 
-
+const { v4: uuidv4 } = require("uuid");
 const weekdays = moment.weekdaysShort();
 
+const rows = 6;
+const columns = 7;
+
 const Day = ({ num }: { num: number }) => (
-  <div id="u3092" className="btn">
-    <div id="u3092_div" className="day"></div>
-    <div id="u3092_text" className="text ">
-      <p id="cache2">
-        <span id="cache3">{12}</span>
-      </p>
-    </div>
+  <div id={v4()} className="day hand">
+    <span id={`day_${v4()}`} className="text">
+      {num}
+    </span>
+    <span id={`frac_${v4()}`} className="text2">
+      4/16
+    </span>
   </div>
 );
 
-export const DatePicker: React.FC<{}> = ({}) => (
-  <div>
-     {weekdays.map(e=><div>{e}</div>)} 
-     <div>NIck</div>
-    <Day num={13}></Day>
+const CalendarBody = ({
+  daysInMonth,
+  weekday,
+  count = 0,
+}: {
+  daysInMonth: number;
+  weekday: number;
+  count?: number;
+}) => (
+  <div id={`${v4()}`} className="rTable">
+    <div id={`${v4()}`} className="rTableRow">
+      {weekdays.map((e) => (
+        <div id={`${v4()}`} className="rTableCell">
+          {e}
+        </div>
+      ))}
+    </div>
+
+    {Array.from({ length: rows }, (e) =>
+      Array.from({ length: columns }, (e) => 0)
+    )
+      .map((row, i) =>
+        row.map((e, j) =>
+          row.length * i + j >= weekday && daysInMonth > count ? ++count : e
+        )
+      )
+      .map((row) => (
+        <div id={`${v4()}`} className="rTableRow">
+          {row.map((e) =>
+            e > 0 ? (
+              <div id={`${v4()}`} className="rTableCell">
+                <Day num={e}></Day>
+              </div>
+            ) : (
+              <div id={`${v4()}`} className="rTableCell">
+                <div className="hidden">{}</div>
+              </div>
+            )
+          )}
+        </div>
+      ))}
   </div>
 );
+
+const m = moment();
+
+export const DatePicker: React.FC<{}> = ({}) => {
+  const [value, setValue] = useState<number>(m.daysInMonth());
+
+  return (
+    <div className="rTable">
+      <div className="rTable">
+        <div className="rTableRow">
+          <div className="rTableCell">
+            <div className="flex-container">
+              <span
+                onClick={() =>
+                  setValue(
+                    m.subtract(1, "months").startOf("month").daysInMonth()
+                  )
+                }
+              >
+                <h3 className="hand">{"<"}</h3>
+              </span>
+              <h3>{m.format("MMM") + " " + m.format("Y")}</h3>
+
+              <span
+                onClick={() =>
+                  setValue(m.add(1, "months").startOf("month").daysInMonth())
+                }
+              >
+                <h3 className="hand">{">"}</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <CalendarBody
+          daysInMonth={value}
+          weekday={m.add(0, "days").startOf("month").day()}
+        ></CalendarBody>
+      </div>
+    </div>
+  );
+};
